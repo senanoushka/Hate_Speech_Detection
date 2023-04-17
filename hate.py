77aaa4b9293f4b002a058393c16367880642627e
Anoushka Sen <senanoushka08@gmail.com>
2023-04-17 12:01:04 +0530

    Update hate.py

--- hate.py
+++ hate.py
@@ -1,4 +1,4 @@
-from nltk.util import pr
+from nltk.probability import FreqDist, ConditionalFreqDist
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
